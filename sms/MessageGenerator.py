0e38c733becb9cd0e07c45c5613ae1fe1ea70987
Adam Feuer <adamf@pobox.com>
2011-12-23 11:33:21 -0800

    added test and method for getDaysBetweenDates

--- sms/MessageGenerator.py
+++ sms/MessageGenerator.py
@@ -1,4 +1,27 @@
+from datetime import time, datetime, timedelta
 
+class DaySegment:
+   def __init__(self, startDateTime, segmentLength):
+      self.segmentLength = segmentLength
+      self.start = startDateTime
+      self.end = startDateTime + segmentLength
+      self.dayStart = None
+      self.dayEnd = None
 
 class MessageGenerator:
-   pass
+   def __init__(self):
+      pass
+
+   def getDaySegments(self, startDateTime, endDateTime):
+      segment = DaySegment(startDateTime, endDateTime-startDateTime)
+      return [segment, segment, segment]
+
+
+   def getDaysBetweenDates(self, startDateTime, endDateTime):
+      thisDay = datetime(startDateTime.year, startDateTime.month, startDateTime.day)
+      oneDay = timedelta(1)
+      days = 0
+      while (thisDay < endDateTime):
+         thisDay += oneDay
+         days += 1
+      return days - 1
--- sms/MessageGeneratorTestCase.py
+++ sms/MessageGeneratorTestCase.py
@@ -1,8 +1,42 @@
+from datetime import time, datetime, timedelta
+
 from django.utils import unittest
+from sms.MessageGenerator import MessageGenerator, DaySegment
+
 
 class MessageGeneratorTestCase(unittest.TestCase):
+
+   def setUp(self):
+      self.messageGenerator = MessageGenerator()
 
    def test_nothing(self):
       self.assertEqual(True, True, "True is not equal to True!")
 
-  
+   def testGetDaySegments(self):
+      startDateTime = datetime.now()
+      threeDays = 3
+      surveyLength = timedelta(threeDays)
+      endDateTime = startDateTime + surveyLength
+      messageGenerator = MessageGenerator()
+      result = messageGenerator.getDaySegments(startDateTime, endDateTime)
+      self.assertIsNotNone(result, "result should not return None!")
+      self.assertEqual(3, len(result), "result should contain exactly 3 segments!")
+      self.assertEqual(startDateTime, result[0].start, "result[0] should have a start.")
+
+   def testGetDaysBetweenDates(self):
+      startDateTime = datetime.now()
+      surveyLengthInDays = 1
+      self.verifyDaysBetweenDates(startDateTime, surveyLengthInDays)
+      surveyLengthInDays = 2
+      self.verifyDaysBetweenDates(startDateTime, surveyLengthInDays)
+      surveyLengthInDays = 3
+      self.verifyDaysBetweenDates(startDateTime, surveyLengthInDays)
+
+   def verifyDaysBetweenDates(self, startDateTime, surveyLengthInDays):
+      surveyLength = timedelta(surveyLengthInDays)
+      endDateTime = startDateTime + surveyLength
+      result = self.messageGenerator.getDaysBetweenDates(startDateTime, endDateTime)
+      self.assertEqual (surveyLengthInDays, result, "result should be exactly %d days, but was %d"  % (surveyLengthInDays, result))
+      
+      
+      
