a16525dfb701bd61dbc04d1785f5740b0add608c
Adam Feuer <adamf@pobox.com>
2011-12-24 16:38:39 -0800

    added a single test to verify the start of a DaySegment

--- sms/MessageGenerator.py
+++ sms/MessageGenerator.py
@@ -1,32 +1,39 @@
 from datetime import time, datetime, timedelta
 
 class DaySegment:
-   def __init__(self, startDateTime, segmentLength):
+   def __init__(self, startDateTime, segmentLength, dayStart, dayEnd):
       self.segmentLength = segmentLength
       self.start = startDateTime
-      self.end = startDateTime + segmentLength
-      self.dayStart = None
-      self.dayEnd = None
+      self.end = startDateTime + segmentLength - timedelta(days=0, seconds=1)
+      self.dayStart = datetime(self.start.year, self.start.month, self.start.day, dayStart.hour, dayStart.minute)
+      self.dayEnd  = datetime(self.start.year, self.start.month, self.start.day, dayEnd.hour, dayEnd.minute)
+      self.dayLength = self.dayEnd - self.dayStart
 
 class MessageGenerator:
    def __init__(self):
       pass
 
-   def getDaySegmentsForDates(self, startDateTime, endDateTime):
+   def getDaySegmentsForDates(self, startDateTime, endDateTime, dayStart, dayEnd):
       segments = []
       thisDay = datetime(startDateTime.year, startDateTime.month, startDateTime.day)
-      oneDay = timedelta(1)
-      days = 0
+      oneDay = timedelta(days = 1)
       while (thisDay < endDateTime):
-         newSegment = DaySegment(thisDay, oneDay)
+         newSegment = DaySegment(thisDay, oneDay, dayStart, dayEnd)
          thisDay += oneDay
-         days += 1
          segments.append(newSegment)
+      self.fixStart(segments[0])
+      self.fixEnd(segments[-1])
       return segments
+
+   def fixStart(self, segment):
+      pass
+
+   def fixEnd(self, segment):
+      pass
 
    def getDaysBetweenDates(self, startDateTime, endDateTime):
       thisDay = datetime(startDateTime.year, startDateTime.month, startDateTime.day)
-      oneDay = timedelta(1)
+      oneDay = timedelta(days = 1)
       days = 0
       while (thisDay < endDateTime):
          thisDay += oneDay
--- sms/MessageGeneratorTestCase.py
+++ sms/MessageGeneratorTestCase.py
@@ -9,41 +9,64 @@ class MessageGeneratorTestCase(unittest.TestCase):
    def setUp(self):
       self.messageGenerator = MessageGenerator()
 
-   def test_nothing(self):
-      self.assertEqual(True, True, "True is not equal to True!")
-
-   def testGetDaySegmentsForDatesMidnight(self):
-      startDateTime = datetime(2011, 12, 23, 0, 0, 0)
-      self.verifyDaySegmentsForDates(startDateTime, surveyLengthInDays=1, expectedResult=1)
-      self.verifyDaySegmentsForDates(startDateTime, surveyLengthInDays=2, expectedResult=2)
-      self.verifyDaySegmentsForDates(startDateTime, surveyLengthInDays=3, expectedResult=3)
-
-   def testGetDaySegmentsForDatesNotMidnight(self):
-      startDateTime = datetime(2011, 12, 23, 11, 23, 01)
-      self.verifyDaySegmentsForDates(startDateTime, surveyLengthInDays=1, expectedResult=2)
-      self.verifyDaySegmentsForDates(startDateTime, surveyLengthInDays=2, expectedResult=3)
-      self.verifyDaySegmentsForDates(startDateTime, surveyLengthInDays=3, expectedResult=4)
-
    def testGetDaysBetweenDatesStartMidnight(self):
       startDateTime = datetime(2011, 12, 23, 0, 0, 0)
-      self.verifyDaysBetweenDates(startDateTime, surveyLengthInDays=1, expectedResult=1)
-      self.verifyDaysBetweenDates(startDateTime, surveyLengthInDays=2, expectedResult=2)
-      self.verifyDaysBetweenDates(startDateTime, surveyLengthInDays=3, expectedResult=3)
+      self.verifyDaysBetweenDates(startDateTime, surveyLengthInDays=1, expectedNumberOfDays=1)
+      self.verifyDaysBetweenDates(startDateTime, surveyLengthInDays=2, expectedNumberOfDays=2)
+      self.verifyDaysBetweenDates(startDateTime, surveyLengthInDays=3, expectedNumberOfDays=3)
 
    def testGetDaysBetweenDatesStartNotMidnight(self):
       startDateTime = datetime(2011, 12, 23, 9, 30, 0)
-      self.verifyDaysBetweenDates(startDateTime, surveyLengthInDays=1, expectedResult=2)
-      self.verifyDaysBetweenDates(startDateTime, surveyLengthInDays=2, expectedResult=3)
-      self.verifyDaysBetweenDates(startDateTime, surveyLengthInDays=3, expectedResult=4)
+      self.verifyDaysBetweenDates(startDateTime, surveyLengthInDays=1, expectedNumberOfDays=2)
+      self.verifyDaysBetweenDates(startDateTime, surveyLengthInDays=2, expectedNumberOfDays=3)
+      self.verifyDaysBetweenDates(startDateTime, surveyLengthInDays=3, expectedNumberOfDays=4)
+      self.verifyDaysBetweenDates(startDateTime, surveyLengthInDays=7, expectedNumberOfDays=8)
 
-   def verifyDaysBetweenDates(self, startDateTime, surveyLengthInDays, expectedResult):
+   def testGetDaySegmentsForDatesMidnight(self):
+      startDateTime = datetime(2011, 12, 23, 0, 0, 0)
+      self.verifyDaySegmentsForDates(startDateTime, surveyLengthInDays=1,
+                                     expectedNumberOfDays=1)
+      self.verifyDaySegmentsForDates(startDateTime, surveyLengthInDays=2,
+                                     expectedNumberOfDays=2)
+      self.verifyDaySegmentsForDates(startDateTime, surveyLengthInDays=3,
+                                     expectedNumberOfDays=3)
+
+   def testGetDaySegmentsContentsForDatesMidnight(self):
+      dayStart = time(hour = 9, minute = 0, second = 0)
+      dayEnd = time(hour = 21, minute = 0, second = 0)
+      startDateTime = datetime(2011, 12, 23, 2, 10, 0)
+      surveyLengthInDays=1
+      expectedNumberOfDays=2
+      surveyLength = timedelta(surveyLengthInDays)
+      endDateTime = startDateTime + surveyLength
+      dayStart = time(hour = 0, minute = 0, second = 0)
+      dayEnd = time(hour = 23, minute = 59, second = 59)
+      result = self.messageGenerator.getDaySegmentsForDates(startDateTime, endDateTime, dayStart, dayEnd)
+      self.assertEqual(expectedNumberOfDays, len(result), "result should contain exactly %d DaySegments, but instead contained %d [%s]"  % (expectedNumberOfDays, len(result), result))
+      segment = result[0]
+      expectedDayStart = datetime(startDateTime.year, startDateTime.month, startDateTime.day, dayStart.hour, dayStart.minute)
+      self.assertEqual(expectedDayStart, segment.dayStart, "dayStart should be identical: %s - %s" % (dayStart, segment.dayStart))
+
+
+   def testGetDaySegmentsForDatesNotMidnight(self):
+      startDateTime = datetime(2011, 12, 23, 11, 23, 01)
+      self.verifyDaySegmentsForDates(startDateTime, surveyLengthInDays=1,
+                                     expectedNumberOfDays=2)
+      self.verifyDaySegmentsForDates(startDateTime, surveyLengthInDays=2,
+                                     expectedNumberOfDays=3)
+      self.verifyDaySegmentsForDates(startDateTime, surveyLengthInDays=3,
+                                     expectedNumberOfDays=4)
+
+   def verifyDaysBetweenDates(self, startDateTime, surveyLengthInDays, expectedNumberOfDays):
       surveyLength = timedelta(surveyLengthInDays)
       endDateTime = startDateTime + surveyLength
       result = self.messageGenerator.getDaysBetweenDates(startDateTime, endDateTime)
-      self.assertEqual(expectedResult, result, "result should be exactly %d days, but was %d"  % (expectedResult, result))
+      self.assertEqual(expectedNumberOfDays, result, "result should be exactly %d days, but was %d"  % (expectedNumberOfDays, result))
       
-   def verifyDaySegmentsForDates(self, startDateTime, surveyLengthInDays, expectedResult):
+   def verifyDaySegmentsForDates(self, startDateTime, surveyLengthInDays, expectedNumberOfDays):
       surveyLength = timedelta(surveyLengthInDays)
       endDateTime = startDateTime + surveyLength
-      result = self.messageGenerator.getDaySegmentsForDates(startDateTime, endDateTime)
-      self.assertEqual(expectedResult, len(result), "result should contain exactly %d DaySegments, but instead contained %d [%s]"  % (expectedResult, len(result), result))
+      dayStart = time(hour = 0, minute = 0, second = 0)
+      dayEnd = time(hour = 23, minute = 59, second = 59)
+      result = self.messageGenerator.getDaySegmentsForDates(startDateTime, endDateTime, dayStart, dayEnd)
+      self.assertEqual(expectedNumberOfDays, len(result), "result should contain exactly %d DaySegments, but instead contained %d [%s]"  % (expectedNumberOfDays, len(result), result))
