dd4770240331e9b74891ac0c64865f8e3c47a9e7
Adam Feuer <adamf@pobox.com>
2012-01-01 22:51:00 -0800

    refactored constant in cron

--- sms/cron.py
+++ sms/cron.py
@@ -6,12 +6,14 @@ from SmsSender import SmsSender, SmsStatus
 
 logger = logging.getLogger(__name__)
 
+TOO_OLD_MINUTES = 5
+
 @cronjobs.register
 def send_messages():
    message_sender = SmsSender()
    now = datetime.datetime.now()
    now_string = now.strftime("%Y-%m-%d %H:%M")
-   max_timedelta = datetime.timedelta(minutes = 5)
+   max_timedelta = datetime.timedelta(minutes = TOO_OLD_MINUTES)
    messages = Message.objects.filter(send_at__lte=now_string).exclude(sent=True).order_by('send_at')
    for message in messages:
       delta = now - message.send_at
