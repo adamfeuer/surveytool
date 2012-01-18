414d273069cc5c32a9321836dced6b226572a722
Adam Feuer <adamf@pobox.com>
2012-01-18 04:26:14 +0000

    updated staging send messages cron with correct settings for staging

--- bin/sendmessages-cron-staging.sh
+++ bin/sendmessages-cron-staging.sh
@@ -2,5 +2,5 @@
 SURVEYTOOL_ENV=/opt/webapps/research-staging.liveingreatness.com
 source $SURVEYTOOL_ENV/bin/activate
 cd $SURVEYTOOL_ENV/surveytool
-python $SURVEYTOOL_ENV/surveytool/manage.py cron send_messages --settings=surveytool.conf.prod.settings
+python $SURVEYTOOL_ENV/surveytool/manage.py cron send_messages --settings=surveytool.conf.staging.settings
 
